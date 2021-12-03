2c02b86866fc3fcac318218b202caf5e2dea7505
Swopnil Bista <56784245+Swopnil38@users.noreply.github.com>
2021-12-03 12:49:42 +0545

    Deleted lab1.py

--- lab1.py
+++
@@ -1,7 +0,0 @@
-#sum of three number
-
-num1 = int(input("Enter First Number: "))
-num2 = int(input("Enter Second Number: "))
-num3 = int(input("Enter Third Number: "))
-sum = num1 + num2 + num3
-print ("The Sum of ",num1,",",num2,"&",num3,"is:{}.".format(sum))
