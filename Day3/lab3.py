7df088ea2a5aad5f5aba725eb32657df05874624
Swopnil38 <swopnilbista2003@gmail.com>
2021-12-06 17:25:22 +0545

    Added lab 3 Day 3

---
+++ Day3/lab3.py
@@ -0,0 +1,8 @@
+#Check whether the user input number is even or odd and display it to user. 
+number = int(input("Enter Any Number: "))
+a = number%2
+if a == 0:
+    print("The Number is Odd.")
+else:
+    print("THe Number is Even.")
+                   
