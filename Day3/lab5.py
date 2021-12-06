fd1043a08058c05a108466fb8b27be1b8de4e4ac
Swopnil38 <swopnilbista2003@gmail.com>
2021-12-06 17:27:13 +0545

    Added lab 5 Day 3

---
+++ Day3/lab5.py
@@ -0,0 +1,8 @@
+#For given integer x, print ‘True’ if it is positive, print ‘False’ if it is negative and print ‘zero’ if it is 0. 
+num = int(input("Enter any Number:"))
+if num>0:
+    print("True")
+elif num<0:
+    print("False")
+else:
+    print("zero")
