59683f11d90427100fd7549827993fa7201a8a80
Swopnil38 <swopnilbista2003@gmail.com>
2021-12-15 17:25:49 +0545

    Added Day5 lab4

---
+++ Day5/lab4.py
@@ -0,0 +1,15 @@
+#Write a Python program to construct the following pattern, using a nested for loop.
+for i in range(1,6):
+    for j in range(i):
+        print("*",end=" ")
+    print(sep="\n")
+k = 4
+while True:
+    for j in range(k):
+        print("*",end=" ")
+    print(sep="\n")
+    k = k-1
+    if k ==0:
+        break
+    
+
