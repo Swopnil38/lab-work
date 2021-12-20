c0ea2cc3f8b6ee955ebb9c2e1b366ec7acea1193
Swopnil38 <swopnilbista2003@gmail.com>
2021-12-20 19:26:06 +0545

    Updated Set lab 5

--- Day6/Set/lab5.py
+++ Day6/Set/lab5.py
@@ -1,8 +1,9 @@
 #Write a Python program to remove an item from a set if it is present in the set.  
 integers = {1,2,3,4,5}
+a = int(input("Enter a number to remove from set if it is present: "))
 for item in integers:
-    if item == 6:
-        integers.remove(6)
-    else:
-        continue
+    if item == a:
+        integers.remove(a)
+        break
+    
 print(integers)
